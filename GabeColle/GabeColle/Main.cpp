fd1e481357ebf16925ab9717c5c2977c9370c902
Jumpaku <itzimeru@gmail.com>
2015-09-02 19:35:05 +0900

    画面の大きさを1366x768の仮想フルスクリーンに変更

--- GabeColle/GabeColle/Main.cpp
+++ GabeColle/GabeColle/Main.cpp
@@ -2,18 +2,19 @@
 # include <Siv3D.hpp>
 
 #include"Source\Scene\Sample\Sample.h"
+#include"Source\Scene\Start\Start.h"
 
 void Main()
 {
+	//Window::SetVirtualFullscreen(Point(1366, 768));
+	Window::Maximize();
 	SceneManager<String, GameData> manager(SceneManagerOption::None);
-
-	// SceneManagerOption::ShowSceneName を外せばシーン名のデバッグ表示は消える
-	//MyApp manager;
 
 	// フェードイン・アウト時の色
 	manager.setFadeColor(Palette::White);
 
 	// シーンを設定
+	manager.add<Start>(L"Start");
 	manager.add<Sample>(L"Sample");
 
 	while (System::Update()) {
