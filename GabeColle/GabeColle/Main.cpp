d1b5df70d39b88c81ca4bc54291a343817c820be
Hatanas-SUB <hatanas726@gmail.com>
2015-09-04 13:29:26 +0900

    main関数を変更
    あとで元に戻す

--- GabeColle/GabeColle/Main.cpp
+++ GabeColle/GabeColle/Main.cpp
@@ -2,13 +2,14 @@
 # include <Siv3D.hpp>
 
 #include"Source\Scene\Sample\Sample.h"
-#include"Source\Scene\Start\Start.h"
+#include "Source\Scene\Game1\Game1.h"
+//#include"Source\Scene\Start\Start.h"
 
 void Main()
 {
-	Window::SetStyle(WindowStyle::NonFrame); 
-	Window::Resize(1280, 720);
-	Window::Centering();
+	//Window::SetStyle(WindowStyle::NonFrame); 
+	//Window::Resize(1280, 720);
+	//Window::Centering();
 
 	SceneManager<String, GameData> manager(SceneManagerOption::None);
 
@@ -16,7 +17,8 @@ void Main()
 	manager.setFadeColor(Palette::White);
 
 	// シーンを設定
-	manager.add<Sample>(L"Sample");
+	//manager.add<Sample>(L"Sample");
+	manager.add<Game1>(L"Game1");
 
 	while (System::Update()) {
 		if (!manager.updateAndDraw())
