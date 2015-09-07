1f3fd071e9d13ed1fd3de4e3f3d445a1ed8bf0c4
Hatanas-SUB <hatanas726@gmail.com>
2015-09-07 16:07:06 +0900

    Game1クラスがほぼ出来た
    Out of Memory になるとエラー終了します．

--- GabeColle/GabeColle/Source/Scene/Game1/Game1.h
+++ GabeColle/GabeColle/Source/Scene/Game1/Game1.h
@@ -15,13 +15,22 @@ class Game1 :
 	public SceneManager<String, GameData>::Scene
 {
 private:
+	static const double MEMORY_RADIUS;
+	static const double ROOT_RADIUS;
+	static const int POSITION_NUM_X;
+	static const int POSITION_NUM_Y;
+	static const int POSITION_MARGIN_X;
+	static const int POSITION_MARGIN_Y;
+private:
 	gc::Memory<CircleObject> memory_m;
+	std::list<Vec2> positionList_m;
 public:
 	Game1();
 	void init() override;
 	void update() override;
 	void draw() const override;
 private:
+	void initializePositionList();
 	void drawMemory() const;
 	std::vector<int> getExistAddress();
 	void randomLink(int allocAddress);
