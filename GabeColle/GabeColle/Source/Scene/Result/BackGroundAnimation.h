79d6a27a731368a328df86dbec39fef108daa58f
keisuke <keisuke495500@gmail.com>
2015-09-10 15:15:18 +0900

    背景の制御をBackGroundAnimationクラスにした

--- GabeColle/GabeColle/Source/Scene/Result/BackGroundAnimation.h
+++ GabeColle/GabeColle/Source/Scene/Result/BackGroundAnimation.h
@@ -6,9 +6,17 @@
 
 class BackGroundAnimation{
 private:
+	Sound BGM_m;
+	Texture texture_m; // resultBG2 ’˜ŽÒ:/\lex
+	int BGVelocity_m;
+	int bg_x_m;
+	int bg_x2_m;
+
 	ExtendArrowEffect* arrows[10];
 	ExpandCircleEffect* circles[10];
 	Array<ExpandCircleEffect>* tree[3];
 public:
 	BackGroundAnimation();
+	void update();
+	void draw()const;
 };
--- GabeColle/GabeColle/Source/Scene/Result/Result.h
+++ GabeColle/GabeColle/Source/Scene/Result/Result.h
@@ -12,6 +12,7 @@
 #include "StageNameNode.h"
 #include "RankingWindow.h"
 #include "Record.h"
+#include "BackGroundAnimation.h"
 
 class Result :
 	public SceneManager<String, GameData>::Scene
@@ -22,22 +23,13 @@ private:
 	const Texture backGround;
 	const Font resultText;
 	const Font stageNameText;
-	const Sound BGM;
-
-	const Texture texture; // resultBG2 ’˜ŽÒ:/\lex
-	const Texture texture2;
-
-	Circle circle_stage_m = Circle(Window::Center(),circleRadius);
-	Circle circle_time_m = Circle(circle_stage_m.center.x/2,circle_stage_m.center.y/2, circleRadius);
-	Circle circle_error_m = Circle((Window::Width() + circle_stage_m.center.x) / 2, circle_time_m.y, circleRadius);
-	Circle circle_process_m = Circle(circle_time_m.x, (Window::Height() + circle_stage_m.center.y) / 2, circleRadius);
-	Circle circle_totalScore_m = Circle(circle_error_m.x, circle_process_m.y, circleRadius);
+	
+	BackGroundAnimation bg;
+	
 	ResultNode* results_m[4];
 	StageNameNode* stageNode_m;
 
-	int BGVelocity;
-	int BG_X;
-	int BG_X2;
+	
 
 public:
 	Result();
